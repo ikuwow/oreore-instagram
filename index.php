e265fc2abe0a6f30a87ea2fd468c27a17486ca71
Ikuo Degawa <degawa@ikuwow.com>
2016-01-19 22:27:16 +0900

    Add bootstrap3
    its compromise

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="/main.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
 
@@ -30,7 +31,12 @@
   <a href="/login.php">Login with Instagram</a>
 <?php endif;?>
 
+<footer>
+  ikuwow All rights reserved. <a href="/privacy.php">Privacy policy</a>
+</footer>
+
 <script src="main.js"></script>
 <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>
